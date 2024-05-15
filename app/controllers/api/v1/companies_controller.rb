f9bf2c5cb0a60765c9b02fd4747427287092ad66
madmatvey <potehin@gmail.com>
2024-05-15 17:30:25 +0400

    rubocop

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::CompaniesController < ApplicationController
-  def index
-    companies = Company.all.order(created_at: :desc)
-    render json: companies.as_json(include: :deals)
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CompaniesController < ApplicationController
+      def index
+        companies = Company.all.order(created_at: :desc)
+        render json: companies.as_json(include: :deals)
+      end
+    end
   end
 end
